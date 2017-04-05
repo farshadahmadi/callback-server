895c0fce2c7480183faec0dfee609f365f9982ec
Farshad Ahmadi Ghohandizi <farshadahmadi@users.noreply.github.com>
2017-04-05 11:29:03 +0200

    Change to the code I know

--- index.js
+++ index.js
@@ -1,88 +1,19 @@
-var http        = require('http'),
-    querystring = require('querystring'),
-    subtext     = require('subtext'),
-    fs          = require('fs'),
-    logFile     = process.env.LOG_FILE || __dirname + '/callbacks.log',
-    port        = process.env.PORT || 8080;
+var express = require('express')
+var app = express()
+var spawn = require('child_process').spawn;
 
-http.createServer(function (req, res) {
-  subtext.parse(req, null, {parse: true, output: 'data'}, function (err, parsed) {
-    var body = parsed.payload;
+app.get('/', function (req, res) {
+  /*var child = spawn('ls', ["-l"]);
+  child.stdout.on("data", function(data){
+    var str = String(data);
+    console.log(str);
+    res.send('Hello Farshad!');
+  });*/
+  console.log(__dirname);
+  res.send('Hello Farshad!');
+  
+})
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("we are cool!");
-
-    var logData = [
-      '> Log time: ' + logTime() + '',
-      '[Method]: ' + req.method,
-      '[Request]: ' + req.url,
-      '[Headers]:', formatObject(req.headers),
-      '[Body]:', formatObject(body),
-      '---------------------------\n'
-    ];
-
-    fs.appendFile(logFile, logData.join('\n'), function (error) {
-      if (error) {
-        console.error(error);
-      }
-    });
-  });
-}).listen(port);
-
-console.log('Now logging on http://127.0.0.1:' + port)
-
-// Functions
-function logTime () {
-  var currentDate = new Date();
-
-  return [
-    currentDate.getFullYear(),
-    ensureDoubleDigit((currentDate.getMonth() + 1)),
-    ensureDoubleDigit(currentDate.getDate())
-  ].join('-') + ' ' + [
-    ensureDoubleDigit(currentDate.getHours()),
-    ensureDoubleDigit(currentDate.getMinutes()),
-    ensureDoubleDigit(currentDate.getSeconds())
-  ].join(':');
-}
-
-function ensureDoubleDigit (number) {
-  if (number < 10) {
-    number = '0' + number;
-  }
-
-  return number;
-}
-
-function formatObject (object, prefix) {
-  if (!object) {
-    return 'None';
-  }
-
-  if (typeof object === 'string') {
-    return object;
-  }
-
-  prefix = prefix || '  -';
-
-  var formatted = [];
-
-  Object.getOwnPropertyNames(object).forEach(function (property) {
-    var value           = object[property],
-        formattedString = prefix;
-
-    if (!Array.isArray(object)) {
-      formattedString += property + ': ';
-    } else if ('length' === property) {
-      return;
-    }
-
-    if (typeof value === 'object') {
-      value = '\n' + formatObject(value, ' ' + prefix);
-    }
-
-    formatted.push(formattedString + value);
-  });
-
-  return formatted.join('\n');
-}
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
