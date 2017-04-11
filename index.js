c115d493d5b768b519b11d766e769111fa56b43b
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2017-04-11 18:07:45 +0300

    Now app is able to read the body request

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next){
+/*app.use(function(req, res, next){
   console.log('method: ' + req.method);
   console.log('query: ' + JSON.stringify(req.query));
   console.log('params: ' + JSON.stringify(req.params));
   console.log('body: ' + JSON.stringify(req.body));
   next();
-});
+});*/
 
 app.get('/', function (req, res) {
   res.status(200).send();
-})
+});
+
+app.post('/', function (req, res) {
+  console.log('body: ' + JSON.stringify(req.body));
+  res.status(200).send();
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
