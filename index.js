cd30ff29eb946ca6643c083c787f3a92acb2ff6f
Farshad Ahmadi Ghohandizi <farshadahmadi@users.noreply.github.com>
2017-04-11 17:16:54 +0300

    Print req body

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 var express = require('express')
 var app = express()
-var spawn = require('child_process').spawn;
 
 app.get('/', function (req, res) {
-  /*var child = spawn('ls', ["-l"]);
-  child.stdout.on("data", function(data){
-    var str = String(data);
-    console.log(str);
-    res.send('Hello Farshad!');
-  });*/
-  console.log(__dirname);
+  console.log(req.body);
   res.status(200).send();
-  
 })
 
 app.listen(8080, function () {
