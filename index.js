7b6db0ffbae303cbe9300cb44352d8265f59b20d
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2017-04-21 12:31:36 +0300

    remove a bug

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.post('/', function (req, res) {
   console.log('body: ' + JSON.stringify(req.body));
   var requestId = req.body.responses[0].requestId;
   console.log(requestId);
-  req.pipe(request.post({url: routingTable[requestId], timeout: 5000})).pipe(res);
-  //res.status(200).send();
+  req.pipe(request.post({url: routingTable[requestId], timeout: 15000}));//.pipe(res);
+  res.status(200).send();
 });
 
 app.post('/register', function (req, res) {
